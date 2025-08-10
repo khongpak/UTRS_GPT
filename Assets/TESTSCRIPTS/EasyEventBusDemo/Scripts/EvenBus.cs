2d6c31a897db1a82e96eaf9c305d359eb5a9f9f7
KHONGPAKLAPTOP\khong <afexim5@gmail.com>
2025-08-10 14:35:17 +0700

    Add File

---
+++ Assets/TESTSCRIPTS/EasyEventBusDemo/Scripts/EvenBus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GameDevTV.RTS.EasyEventBusDemo
+{
+    public static class EventBus
+    {
+        // TODO: เตรียมที่เก็บผู้ฟังตามชนิดเหตุการณ์
+        // เช่น Dictionary<Type, List<Delegate>>
+
+        public static void Register<T>(Action<T> listener) where T : IGameEvent
+        {
+            // TODO: บันทึก listener สำหรับชนิดเหตุการณ์ T
+        }
+
+        public static void Unregister<T>(Action<T> listener) where T : IGameEvent
+        {
+            // TODO: ถอด listener สำหรับชนิดเหตุการณ์ T
+        }
+
+        public static void Raise<T>(T eventData) where T : IGameEvent
+        {
+            // TODO: แจ้งทุก listener ของชนิดเหตุการณ์ T ด้วย eventData
+        }
+    }
+}
---
+++ Assets/TESTSCRIPTS/EasyEventBusDemo/Scripts/EventListenerExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+namespace GameDevTV.RTS.EasyEventBusDemo
+{
+    public class EventListenerExample : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            // TODO: สมัครฟังเหตุการณ์ที่ต้องการ
+            // ตัวอย่าง: EventBus.Register<UnitDiedEvent>(OnUnitDied);
+        }
+
+        private void OnDisable()
+        {
+            // TODO: ยกเลิกการฟังเหตุการณ์
+            // ตัวอย่าง: EventBus.Unregister<UnitDiedEvent>(OnUnitDied);
+        }
+
+        // ตัวอย่าง handler (แก้ชนิดเหตุการณ์ให้ตรงกับที่ใช้จริง)
+        private void OnUnitDied(/* UnitDiedEvent e */)
+        {
+            // TODO: สิ่งที่อยากทำเมื่อได้รับเหตุการณ์ เช่น Debug.Log หรืออัปเดต UI
+        }
+    }
+}
